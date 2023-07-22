c7d9912b4652d5b83b7f3a89ff754457bcfbbbfa
Nick Iascerinschi <nick.iasceri@gmail.com>
2023-07-22 20:51:51 +0100

    added oh my zsh

--- beautifyterminal.sh
+++ beautifyterminal.sh
@@ -1,4 +1,9 @@
+sh -c -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew uninstall zsh
+brew uninstall romkatv/powerlevel10k/powerlevel10k
 brew install zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 brew install romkatv/powerlevel10k/powerlevel10k
 echo 'source $(brew --prefix powerlevel10k)/powerlevel10k.zsh-theme' >>! ~/.zshrc
 # now install fonts manually
+p10k configure
